0ae6d1d0ac2e8d5b27e43a60f9db496baccfb63f
= <brianwaweru627@gmail.com>
2023-09-14 20:17:51 +0300

    Created Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <>
+            <h1 className="home-app-name">Linki</h1>
+            <h3>Create a Landing Page to Efficiently Share Many links from a Single Page. 🚀</h3>
+            <button className="primary-button">Sign Up</button>
+            <button className="secondary-button">Login</button>
+        </>
+    )
+}
+
+export default Home;
