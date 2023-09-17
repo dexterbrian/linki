30c0f847f2c80d97ae8af7b9bfdbfb0ecd631100
= <brianwaweru627@gmail.com>
2023-09-17 22:23:42 +0300

    Added inputs for login & password

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,12 @@ function Home() {
         <>
             <h1 className="home-app-name">Linki</h1>
             <h3>Create a Landing Page to Efficiently Share Many links from a Single Page. 🚀</h3>
-            <button className="primary-button">Sign Up</button>
-            <button className="secondary-button">Login</button>
+            <div>
+                <input type="text" placeholder="Enter your Username" />
+                <input type="password" placeholder="Enter your Password" />
+            </div>
+            <div><button className="primary-button">Sign Up</button></div>
+            <div><button className="secondary-button">Login</button></div>
         </>
     )
 }
