80e598aba2223e898fa204f6774d0facee43a902
= <brianwaweru627@gmail.com>
2023-09-19 21:44:31 +0300

    Updated Linki.jsx to receive & use props from Home.jsx

--- src/components/Linki.jsx
+++ src/components/Linki.jsx
@@ -1,18 +1,14 @@
 import { useState } from 'react';
 
-function Linki() {
-    
-    const [linkTitle, setLinkTitle] = useState('');
-    const [link, setLink] = useState('');
-
+function Linki({ title, setTitle, url, setUrl }) {
     return (
         <>
             <div className="linki-container">
-                <input type="text" placeholder="Enter your Link" value={linkTitle} onChange={(e) => {
-                    setLinkTitle(e.target.value);
+                <input type="text" placeholder="Enter your Link" value={title} onChange={(e) => {
+                    setTitle(e.target.value);
                 }}/>
-                <input type="text" placeholder="Enter a Title for your Link" value={link} onChange={(e) => {
-                    setLink(e.target.value);
+                <input type="text" placeholder="Enter a Title for your Link" value={url} onChange={(e) => {
+                    setUrl(e.target.value);
                 }}/>
             </div>
         </>
