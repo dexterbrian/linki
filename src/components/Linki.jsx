b3947b792eda5f7c4636598224ba75da1b9a9cc3
= <brianwaweru627@gmail.com>
2023-09-19 14:54:32 +0300

    Created Linki.jsx as a component

--- src/components/Linki.jsx
+++ src/components/Linki.jsx
@@ -1,8 +1,20 @@
+import { useState } from 'react';
+
 function Linki() {
+    
+    const [linkTitle, setLinkTitle] = useState('');
+    const [link, setLink] = useState('');
+
     return (
         <>
-            <h1>Hey there! 👋</h1>
-            <h3>Check out my links below. Don't forget to share! 😊</h3>
+            <div className="linki-container">
+                <input type="text" placeholder="Enter your Link" value={linkTitle} onChange={(e) => {
+                    setLinkTitle(e.target.value);
+                }}/>
+                <input type="text" placeholder="Enter a Title for your Link" value={link} onChange={(e) => {
+                    setLink(e.target.value);
+                }}/>
+            </div>
         </>
     );
 }
