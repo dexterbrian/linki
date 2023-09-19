4b3002e175b1bbb97f9f9ea8fe9814e5258a8642
= <brianwaweru627@gmail.com>
2023-09-19 07:35:46 +0300

    Updated styling

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,12 +3,14 @@ function Landing() {
         <>
             <h1 className="home-app-name">Linki</h1>
             <h3>Create a Landing Page to Efficiently Share Many links from a Single Page. 🚀</h3>
-            <div>
+            <div className="login-container">
                 <input type="text" placeholder="Enter your Username" />
-                <input type="password" placeholder="Enter your Password" />
+                {/* <input type="password" placeholder="Enter your Password" /> */}
+                <div className="login-buttons">
+                    <div className="button-container"><button className="primary-button">Sign Up</button></div>
+                    <div className="button-container"><button className="secondary-button">Login</button></div>
+                </div>
             </div>
-            <div><button className="primary-button">Sign Up</button></div>
-            <div><button className="secondary-button">Login</button></div>
         </>
     )
 }
